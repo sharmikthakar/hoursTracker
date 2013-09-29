4f6e7b4fbaf223d6d318a31deb8c61851e95c3c7
Sharmik Thakar <sharmik@outlook.com>
2013-09-29 19:07:24 +0530

    First commit
    initial commit

---
+++ js/hours.js
@@ -0,0 +1,30 @@
+function hoursCtrl($scope){
+   
+   $scope.tasks = [
+      {time: 0}
+   ]
+   
+   $scope.addTask = function(){
+      if ($scope.newTaskHours == undefined){
+         alert("Enter a Non Zero value");
+      }
+      else{
+         $scope.tasks.push({time:$scope.newTaskHours});
+         $scope.newTaskHours = '';
+      }
+   }
+   
+   $scope.totalHours = function(){
+      var totalH = 0;
+      angular.forEach($scope.tasks, function(task){
+         totalH += parseFloat(task.time);
+      });
+      return totalH;
+   }
+   
+   $scope.remove=function(task){ 
+     var index=$scope.tasks.indexOf(task)
+     $scope.tasks.splice(index,1);     
+   }   
+   
+}
---
+++ readme.txt
@@ -0,0 +1,28 @@
+## hoursTracker v1.0
+
+a simple app to testdrive angularjs
+
+get the latest build from: http://github.com/sharmik/hoursTracker
+
+## Features
+
+* simple time calculator
+
+## Contributing
+
+Anyone and everyone is welcome to contribute towards enhancing the features of this project.
+
+
+## Project information
+
+* Source: http://github.com/sharmik/hoursTracker
+* Twitter: http://twitter.com/sharmikthakar
+* Ideas / Found Bug? Log an issue at http://github.com/sharmik/hoursTracker
+
+## Next Build v2
+
+* add standard time hours.
+
+
+## License
+GPL License
