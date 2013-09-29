4e70d27dd884a76109161285e9d0b967818518d1
Sharmik Thakar <sharmik@outlook.com>
2013-09-30 02:22:23 +0530

    some more ui stuff and some validations

--- js/hours.js
+++ js/hours.js
@@ -1,12 +1,18 @@
 function hoursCtrl($scope){
    
    $scope.tasks = [
+      {time: 0},
+      {time: 0},
+      {time: 0},
       {time: 0}
    ]
    
    $scope.addTask = function(){
       if (($scope.newTaskHours == undefined) || ($scope.newTaskHours == 0)){
-         alert("Enter a Non Zero value");
+         alert("Add an actual value of Time!");
+      }
+      else if (($scope.newTaskHours < 0)){
+      alert("How I wish, we could add back in Time!");
       }
       else{
          $scope.tasks.push({time:$scope.newTaskHours});
@@ -20,6 +26,23 @@ function hoursCtrl($scope){
          totalH += parseFloat(task.time);
       });
       return totalH;
+   }
+   
+   $scope.getStatus = function(){
+      var rem = $scope.targetHrs - $scope.totalHours();
+      if (($scope.targetHrs == undefined)||($scope.targetHrs == 0))
+         return 'Fill out <span class="text-primary">hours to be completed</span> to get a countdown';
+      if (rem > 0){
+         return "You need <span class='text-info'><strong>"+rem+"</strong></span> more hours to reach <strong class='text-warning'>"+$scope.targetHrs+"</strong> Hours";
+      }
+      if (rem < 0 ){
+         return 'Too many hours reduce by '+rem*-1+ ' Hours';
+      }
+      if (rem == 0){
+         return "<span class='text-success'>Bingo! you have compelted <strong class='text-success'>"+$scope.targetHrs+"</strong> hours today."
+      }
+      
+      return rem;
    }
    
    $scope.remove=function(task){ 
